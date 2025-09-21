a943c64dcc6e769972cc287cc50378cbc16dac5c
unknown <danil.rakhmatullin.2013@gmail.com>
2025-09-22 01:54:31 +0500

    Создание класса-рлдителя и его полей

--- Lab_rab_RakhmatullinR.I._БПИ-23-01/ComplexNumberCalculator.cs
+++ Lab_rab_RakhmatullinR.I._БПИ-23-01/ComplexNumberCalculator.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Lab_rab_RakhmatullinR.I._БПИ_23_01
 {
-    internal class ComplexNumberCalculator
+    public class ComplexNumberCalculator
     {
+        public double RealPart { get; private set; }      // a1 - действительная часть
+        public double ImaginaryPart { get; private set; } // b1 - мнимая часть
+
+        public ComplexNumberCalculator(double real, double imaginary)
+        {
+            RealPart = real;
+            ImaginaryPart = imaginary;
+        }
+
+
+
     }
 }
