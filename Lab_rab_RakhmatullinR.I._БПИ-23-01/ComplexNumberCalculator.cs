ffd40d437ad932df70a9e0516beb0630412a523e
танчулпан <@LAPTOP-DBQL0IOB>
2025-09-22 01:44:17 +0500

    Добавьте файлы проекта.

---
+++ Lab_rab_RakhmatullinR.I._БПИ-23-01/ComplexNumberCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_rab_RakhmatullinR.I._БПИ_23_01
+{
+    internal class ComplexNumberCalculator
+    {
+    }
+}
